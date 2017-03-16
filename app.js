22c024b77eb3e12ed619fb203a4bc5845eb40e94
GrimmOutlook <grimmoutlook87@gmail.com>
2017-03-16 09:03:53 -0400

    Outline of steps, and first attempt to code them.

---
+++ app.js
@@ -0,0 +1,61 @@
+// $(function() {
+
+
+// First Event:
+  // User inputs text and clicks "add item" button.
+  // After clicking the button, the text entered is stored into a variable
+  $('button').on('click', function(){
+        var userText = $('#shopping-list-entry').val();
+        var userItem = userText.toLowerCase();
+        console.log(userItem);
+  //the user input value (item) gets displayed:
+      // - at the bottom of the list
+      // - with a border around it
+      // - with a check button
+      // - with a delete button
+      $("ul").append("<li>" + userItem + "</li>");
+      $(userItem).addClass('shopping-item-toggle', 'shopping-item-delete');
+
+  });
+
+
+
+
+// Second Event:
+  // User clicks the check button:
+    // If item is currently unchecked, "check" the item.
+    // If item is currently checked, "un-check" the item.
+  $('ul').on('click', 'li', function(event) {
+    $(this).toggleClass('shopping-list__unchecked');
+  });
+
+// Third Event:
+  // User clicks the delete button:
+    // Item & it's border and buttons are permanently deleted.
+
+
+
+  $('ul').on('click', 'li', function(event) {
+    this.remove();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
