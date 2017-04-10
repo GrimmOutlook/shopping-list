fce68bf1000d81b1b0d18a098ca3ae4f13257aa1
GrimmOutlook <grimmoutlook87@gmail.com>
2017-04-10 18:59:37 -0400

    Moved the onclick events for check and delete items to html code.  Wrapped the button submit jQuery fxn in an onload fxn.

--- app.js
+++ app.js
@@ -1,57 +1,48 @@
-// $(function() {
-
-
+// ONLOAD FUNCTION!!  CAN ONLY BE ONE PER JS FILE!!
+$(function() {
 // First Event:
   // User inputs text and clicks "add item" button.
   // After clicking the button, the text entered is stored into a variable
-  $('button').on('click', function(){
+  $('button[type="submit"]').on('click', function(event){
         event.preventDefault();
-        console.log($(this));
+        // console.log($(this));
         var userText = $('#shopping-list-entry').val();
         var userItem = userText.toLowerCase();
-        console.log(userItem);
+        // console.log(userItem);
   //the user input value (item) gets displayed:
       // - at the bottom of the list
       // - with a border around it
       // - with a check button
       // - with a delete button
-      $('ul').append('<li>' + '<span class="shopping-item">' + userItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">' + 'check' + '</span></button><button class="shopping-item-delete"><span class="button-label">' + 'delete' + '</span></button></div></li>')
+      $('ul').append('<li>' + '<span class="shopping-item">' + userItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle" onclick="checkItem(this)"><span class="button-label">' + 'check' + '</span></button><button class="shopping-item-delete" onclick="deleteItem(this)"><span class="button-label">' + 'delete' + '</span></button></div></li>')
   });
+});
 
+
+function checkItem(item) {
 // Second Event:
   // User clicks the check button:
     // If item is currently unchecked, "check" the item.
     // If item is currently checked, "un-check" the item.
-  $('.shopping-item-toggle').on('click', function() {
-    event.preventDefault();
-    console.log($(this).closest('.shopping-item'));
-    $(this).closest('.shopping-item').toggleClass('shopping-item__checked');
-  });
+  // $('.shopping-item-toggle').on('click', function(event) {
+    // event.preventDefault();
+    // debugger
+    var topSibling = $(item).closest('.shopping-item-controls').siblings('.shopping-item');
+    console.log(topSibling);
 
+    $(item).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+  // });
+}
+
+function deleteItem() {
 // Third Event:
   // User clicks the delete button:
     // Item & it's border and buttons are permanently deleted.
-  $('.shopping-item-delete').on('click', function() {
-    this.remove();
+  $('.shopping-item-delete').on('click', function(event) {
+    console.log("Dave");
+    event.preventDefault();
+    $(this).closest('li').remove();
   });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// });
